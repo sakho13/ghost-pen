ca9cae0a8d8bc0ecb7eb1785a2f7af58df9a4018
sakho13 <wecl.khotaro@gmail.com>
2025-07-10 22:33:25 +0900

    🔨 ESLintルールを更新し、命名規則とメンバーアクセスの明示性を強化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,13 +18,56 @@ export default [
 
     rules: {
       "@typescript-eslint/naming-convention": [
-        "warn",
+        "error",
         {
-          selector: "import",
-          format: ["camelCase", "PascalCase"],
+          selector: "variable",
+          format: ["camelCase"],
+        },
+        {
+          selector: "function",
+          format: ["camelCase"],
+        },
+        {
+          selector: "typeLike",
+          format: ["PascalCase"],
+        },
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+          prefix: ["I"],
+        },
+        {
+          selector: "classMethod",
+          modifiers: ["private"],
+          format: ["camelCase"],
+          leadingUnderscore: "require",
+        },
+        {
+          selector: "classProperty",
+          modifiers: ["private"],
+          format: ["camelCase"],
+          leadingUnderscore: "require",
+        },
+        {
+          selector: "parameter",
+          format: ["camelCase"],
+          filter: {
+            regex: "^_",
+            match: false,
+          },
         },
       ],
-
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          accessibility: "explicit",
+          overrides: {
+            constructors: "explicit",
+          },
+        },
+      ],
+      "@typescript-eslint/semi": ["error", "never"],
+      semi: "off",
       curly: "warn",
       eqeqeq: "warn",
       "no-throw-literal": "warn",
